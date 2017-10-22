26e10a23dc2164603a3d1652c1fb2b52277c84de
Timothy Hogan <tim.hogan93@gmail.com>
2017-10-22 22:29:24 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,32 @@
+var uppercase = "HELLO"
+var lowercase = "hello"
+
+
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log("HELLO")
+}
+
+function logWhisper(string) {
+  console.log('hello')
+}
+
+function sayHiToGrandma(string){
+  if ("hello" === string){
+    console.log("I can't hear you")
+}
+ else if ("HELLO" === string) {
+    console.log("YES INDEED")
+}
+  else {
+    console.log("I love you, too")
+  }
+
+  }
